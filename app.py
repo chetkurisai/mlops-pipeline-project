b402994209c5b2931ada38b926077748da274733
saichtkuri <103044606+chetkurisai@users.noreply.github.com>
2026-04-11 15:14:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+from flask import Flask, request, jsonify
 import joblib
+import os
+
+app = Flask(__name__)
 
 model = joblib.load("model.pkl")
 
-pred = model.predict([[5]])
-print("Prediction:", pred[0])
+@app.route("/")
+def home():
+    return "API running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    value = data["input"]
+    prediction = model.predict([[value]])
+    return jsonify({"prediction": float(prediction[0])})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
